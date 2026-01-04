4cf460096d4e84a94ea5987266203040201786ca
Zakir Ahmad Khan <khanzakirkhan723595@gmail.com>
2026-01-05 01:21:49 +0530

    i solved 2 problems in array and 1 grap rotten orange

---
+++ Array/problems/3Sum_closest.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int currSum = nums[i] + nums[left] + nums[right];
+
+                if (abs(target - currSum) < abs(target - closestSum)) {
+                    closestSum = currSum;
+                }
+
+                if (currSum < target) {
+                    left++;
+                } 
+                else if (currSum > target) {
+                    right--;
+                } 
+                else {
+                    return target; // exact match
+                }
+            }
+        }
+        return closestSum;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums={-1,2,1,-4};
+    int target=1;
+    cout<<"Closest sum is: "<<s.threeSumClosest(nums,target)<<endl;
+}
---
+++ Array/problems/Container_With_Most_Water.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+            maxArea = max(maxArea, area);
+
+            // move the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    int main()
+    {
+        vector<int> height = {1,8,6,2,5,4,8,3,7};
+        cout<<"Water contain: "<<maxArea(height)<<endl;
+    }
